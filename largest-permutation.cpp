c5e1ed01c32bc11cff9609211b11f410aee586e5
Peeyush Yadav <peeyushy95@gmail.com>
2015-10-08 17:10:03 +0530

    Greedy

---
+++ largest-permutation.cpp
@@ -0,0 +1,66 @@
+/*
+   (•_•)
+   <) )>
+    / \
+   AUTHOR  : Peeyush Yadav {~_~}
+   Problem : https://www.hackerrank.com/challenges/largest-permutation
+*/
+#include<bits/stdc++.h>
+using namespace std; typedef long long ll; typedef pair<int,int> grp;
+inline ll power(ll a,ll b)     { ll r=1; while(b){ if(b&1) r=r*a    ; a=a*a    ; b>>=1;} return r;}
+inline ll power(ll a,ll b,ll m){ ll r=1; while(b){ if(b&1) r=(r*a)%m; a=(a*a)%m; b>>=1;} return r;}
+void fast(){	
+	#ifdef Megamind
+		freopen("inp.txt","r",stdin);
+		//freopen("out.txt","w",stdout);
+		#define trace(x)            cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+		#define trace2(x,y)         cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+		#define trace3(x,y,z)       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+	#else
+		#define trace(x)
+		#define trace2(x,y)
+		#define trace3(x,y,z)
+	#endif
+	
+	#define fi						first
+	#define se						second
+	#define mp						make_pair
+	#define pb(x)					push_back(x)
+	#define s(x)					scanf("%d",&x);
+	#define sl(x)					scanf("%lld",&x);
+	#define p(x)					printf("%d\n",x);
+	#define f(a,b,c)				for(int a=b;a<c;a++)
+	#define r(a,b,c)				for(int a=b;a>c;a--)
+	#define p2(x,y)					printf("%d %d\n",x,y);
+	#define pl(x)					printf("%lld\n",x);
+	#define pl2(x,y)				printf("%lld %lld\n",x,y);
+	#define p1d(a,n)				for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+	#define p2d(a,n,m)				for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+}
+/*........................................................END OF TEMPLATES.......................................................................*/
+#define sz 123456
+int a[sz],pos[sz];
+int main(){
+	fast();
+	int n,k,tmp;
+	s(n)
+	s(k)
+	
+	f(i,1,1+n){
+		s(a[i]);
+		pos[a[i]] = i;
+	}
+	
+	f(i,1,n+1){
+		if(!k) break;
+		if(a[i] != n+1-i){
+			pos[a[i]] = pos[n-i+1];
+			swap(a[i],a[pos[n-i+1]]);
+			k--;
+		}
+	}
+	f(i,1,n+1) cout<<a[i]<<" ";
+	#ifdef Megamind
+		cerr << "\nTime elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+	#endif
+}  
